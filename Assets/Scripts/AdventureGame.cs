300565c195e246c933bbfb9bfeec2948009d0ede
RodwelJunior09 <rodweljunior@gmail.com>
2020-01-30 23:28:34 -0400

    The Alchemist Quest - Choose your destiny adventure game

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    // Start is called before the first frame update
+    State state;
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageStates();
+    }
+
+    private void ManageStates()
+    {
+        var nextStates = state.GetNextState();
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
